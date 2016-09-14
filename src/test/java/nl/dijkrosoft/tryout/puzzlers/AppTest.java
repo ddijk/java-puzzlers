a243242615149d72995818596353d9a15f03b660
ddijk <dick@dijkrosoft.nl>
2016-09-14 09:45:07 +0200

    initial checking

---
+++ src/test/java/nl/dijkrosoft/tryout/puzzlers/AppTest.java
@@ -0,0 +1,21 @@
+package nl.dijkrosoft.tryout.puzzlers;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
