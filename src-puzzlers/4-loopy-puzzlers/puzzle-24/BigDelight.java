99b18dfe5fd57a129ec3a91f9dd228a424737c82
Dijk, DI (Dick) <Dick.Dijk@rabobank.nl>
2016-09-19 07:59:29 +0200

    analyse mapping from bit-pattern to value

--- src-puzzlers/4-loopy-puzzlers/puzzle-24/BigDelight.java
+++ src-puzzlers/4-loopy-puzzlers/puzzle-24/BigDelight.java
@@ -1,6 +1,13 @@
 class BigDelight {
     public static void main(String[] args) {
+
+        System.out.println(0x90);
+        System.out.println((0x90 & 0xff));
+        System.out.println((byte)(0x90 & 0xff));
+
         for (byte b = Byte.MIN_VALUE; b < Byte.MAX_VALUE; b++) {
+
+            //System.out.println("b val is "+ b+", int val is "+ (int) b);
             if (b == 0x90)
                 System.out.print("Joy!");
         }
